77f24e61529b7a8e23838dbd76498248d11bb688
hikitooo <hcton2426@apcs.fitus.edu.vn>
2025-02-12 20:45:25 +0700

    trie12022025-20:45

---
+++ build/CMakeFiles/DATA_VISUALIZER.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DATA_VISUALIZER.dir/src/main.cpp.obj"
+  "CMakeFiles/DATA_VISUALIZER.dir/src/main.cpp.obj.d"
+  "DATA_VISUALIZER.exe"
+  "DATA_VISUALIZER.exe.manifest"
+  "DATA_VISUALIZER.pdb"
+  "libDATA_VISUALIZER.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DATA_VISUALIZER.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/DATA_VISUALIZER.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DATA_VISUALIZER.
+# This may be replaced when dependencies are built.
