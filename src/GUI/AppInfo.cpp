74e0a3641ac29a4e0e4de3e842a93d7c628b2c19
Bix2llets <nhtphat2418@apcs.fitus.edu.vn>
2025-04-12 21:52:37 +0700

    Adjusted the canvas

--- src/GUI/AppInfo.cpp
+++ src/GUI/AppInfo.cpp
@@ -3,6 +3,6 @@
 namespace AppInfo {
 
 const float DELTA_TIME = 1.0f / 60;
-const Vector2 UPPER_LEFT = {50, 50};
-const Vector2 LOWER_RIGHT = {1550, 650};
+const Vector2 UPPER_LEFT = {65, 55};
+const Vector2 LOWER_RIGHT = {1550, 730};
 }  // namespace AppInfo
